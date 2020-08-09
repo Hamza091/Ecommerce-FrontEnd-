57b9a333d18a06448d6b9163963462609e4eeecb
Hamza <64861717+Hamza091@users.noreply.github.com>
2020-08-09 07:47:04 -0700

    done

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import './App.css';
 import {Data} from './Data';
 import Nav from './Nav';
 import Main from './Main'
-import Check from './Check'
+
 function App() {
   
   return (
    <Data >
      <Nav />
      <Main />
-     <Check />
   </Data> 
    
     
